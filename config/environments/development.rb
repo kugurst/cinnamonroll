b14b5347d3af3e4c234463ac4208712821da5e3e
kugurst <ma2799@gmail.com>
2015-10-18 03:26:39 -0400

    Added AngularJS to the Gemfile
    Also, added thin as a requirement for development.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,10 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  class Application < Rails::Application
+    config.web_console.whitelisted_ips = '108.219.105.233'
+  end
+
   # Do not eager load code on boot.
   config.eager_load = false
 
---
+++ site-notes.md
@@ -0,0 +1,21 @@
+# Site Notes #
+## Design
+
+- Switch backgrounds for each section of my site
+- Transition backgrounds (and also view?)
+
+## Layout
+
+- Sections:
+  - Anime
+  - Projects
+  - Thoughts
+
+### Thoughts
+- Design
+  - Clouds?
+
+- Content
+  - Commentary on things that I've read
+
+### Site wide
