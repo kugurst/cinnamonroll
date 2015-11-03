b571eb89e53f7769948007acb39be29c323fa19b
kugurst <ma2799@gmail.com>
2015-11-03 02:57:12 -0500

    Moving to coffeescript.
    Also refined the encryption scheme. Basically, the client initiates most of the dialogue. See notes.org for more detail.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-
-  before_action :require_aes_key
+  # before_action :require_aes_key!
 
   def hello
     render text: "hello, world!"
@@ -15,7 +14,20 @@ class ApplicationController < ActionController::Base
   BANNER = "********************************"
 
   private
-  def require_aes_key
-    SecurityHelper.require_aes_key session, request
+  def require_aes_key!(ret_point = request.url)
+    set_return_point ret_point
+    SecurityHelper.require_aes_key session, request, self
+  end
+
+  def set_return_point(path, overwrite = false)
+    if overwrite or session[:return_point].blank?
+      session[:return_point] = path
+    end
+  end
+
+  def return_point
+    ret = session[:return_point] || root_path
+    session.delete :return_point
+    ret
   end
 end
