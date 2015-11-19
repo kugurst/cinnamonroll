6523b66ff1afb19fe61db115a90891c4413798eb
kugurst <ma2799@gmail.com>
2015-11-19 00:05:48 -0500

    Completed the SessionController spec.
    No changes necessary to SessionController

--- spec/controllers/session_controller_spec.rb
+++ spec/controllers/session_controller_spec.rb
@@ -1,12 +1,70 @@
 require "rails_helper"
 
+describe SessionController, '#create' do
+  context 'when the user is not in the database' do
+    before :each do
+      @pass = "password"
+      @user = build :user
+    end
+
+    it 'renders #new and sets the status to :not_found' do
+      params = { session: { email_or_username: @user.email, password: @pass }, enc: { active: false } }
+
+
+      post :create, params
+
+
+      expect(response.status).to be 404
+      expect(response).to render_template 'new'
+    end
+  end
+
+  context 'when the user is in the database' do
+    before :each do
+      @pass = "password"
+      @user = create :user
+    end
+
+    context 'when the password is bad' do
+      it 'responds with not found' do
+        params = { session: { email_or_username: @user.email, password: @pass * 2 }, enc: { active: false } }
+
+
+        post :create, params
+
+
+        expect(response.status).to be 404
+        expect(response).to render_template 'new'
+      end
+    end
+
+    context 'when the password is good' do
+      before :each do
+        @pass = "password"
+        @user = create :user
+      end
+
+      it "redirects to the user's page" do
+        params = { session: { email_or_username: @user.email, password: @pass }, enc: { active: false } }
+
+
+        post :create, params
+
+
+        expect(response.status).to be 302
+        expect(response).to redirect_to "/users/#{@user.id}"
+      end
+    end
+  end
+end
+
 describe SessionController, '#new' do
   context 'when no user is logged in' do
     it 'sends us to the log in page' do
       get :new
 
 
-      expect(response).to render_template("new")
+      expect(response).to render_template "new"
     end
   end
 
@@ -15,7 +73,6 @@ describe SessionController, '#new' do
       pass = "password"
       @user = create :user, password: pass
 
-      puts "@user.email: #{@user.email}"
 
       post :create, session: { email_or_username: @user.email, password: pass },
            enc: { active: false }
@@ -29,6 +86,32 @@ describe SessionController, '#new' do
 
 
       expect(response).to redirect_to "/users/#{@user.id}"
+    end
+  end
+end
+
+describe SessionController, '#destroy' do
+  context 'when a user is logged in' do
+    before :each do
+      pass = "password"
+      @user = create :user, password: pass
+
+
+      post :create, session: { email_or_username: @user.email, password: pass },
+           enc: { active: false }
+
+
+      expect(response).to redirect_to "/users/#{@user.id}"
+    end
+
+    it 'logs the user out' do
+      delete :destroy
+
+
+      expect(response).to redirect_to "/"
+      expect(session[:user_id]).to_not be
+      expect(cookies.signed[:user_id]).to_not be
+      expect(cookies.signed[:remember_token]).to_not be
     end
   end
 end
