8abf52a0c2b369cd297dfcb7d380bb413115d72f
kugurst <ma2799@gmail.com>
2015-11-20 17:37:05 -0500

    Switching back to a non-embedded scheme.
    "includes" is a prefix operator on Mongoid's criteria. You can do something like `Post.includes(:comments).find_by` and eager load all the comments. The example in the documentation was merely an example.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,16 +6,38 @@ class Comment
 
   field :body, type: String
   field :deleted, type: Boolean, default: false
+  field :total_comments, type: Integer, default: 1
+  field :nesting_level, type: Integer, default: 0
+  field :post_id, type: BSON::ObjectId
+  field :user_id, type: BSON::ObjectId
   # belongs_to :post, inverse_of: :comment, polymorphic: true, autosave: true
 
   # recursively_embeds_many
-  embeds_many :comments, as: :com_thread, cyclic: true, cascade_callbacks: true
+  embeds_many :comments, as: :com_thread, cyclic: true, cascade_callbacks: true, after_add: :increment_total_and_chain, after_remove: :decrement_total_and_chain
   embedded_in :com_thread, cyclic: true
   # accepts_nested_attributes_for :child_comments, polymorphic: true
 
-  validates :body, presence: true
+  validates :body, :user_id, :post_id, :nesting_level, :total_comments, presence: true
 
   before_destroy :delete_comment
+
+  def post
+    @post ||= Post.find_by id: post_id
+  end
+
+  def post=(post)
+    @post = post
+    set post_id: post.id
+  end
+
+  def user
+    @user ||= User.find_by id: user_id
+  end
+
+  def user=(user)
+    @user = user
+    set user_id: user.id
+  end
 
   def body
     deleted ? DELETED_STR : super
@@ -24,5 +46,15 @@ class Comment
   def delete_comment
     set deleted: true
     false
+  end
+
+  def increment_total_and_chain(com)
+    inc total_comments: 1
+    com_thread.try :increment_total_and_chain, com
+  end
+
+  def decrement_total_and_chain(com)
+    inc total_comments: -1
+    com_thread.try :decrement_total_and_chain, com
   end
 end
