df4ce914411cf0820252c4e4aa3cb07ee3fa7897
kugurst <ma2799@gmail.com>
2015-11-30 23:50:03 -0500

    Need to add the category descriptions

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,7 @@
 module StaticHelper
+  MESSAGE_ARRAY = ['Hey there', 'Nice weather?', 'Good of you to drop by', 'Bonjour', 'puts "Hello"', "Hope you weren't waiting long"]
+
+  def greeting_message
+    MESSAGE_ARRAY.sample
+  end
 end
